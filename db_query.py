698ece1312d5952c6e990a532c146fb0b98ffa94
danya <you@example.com>
2019-01-05 19:56:56 +0300

    fix

--- db_query.py
+++ db_query.py
@@ -3,5 +3,5 @@ def connection_string():
 
 
 def insert_data_into_db(buy_in, prizepool, place, created_on, db):
-    insert = db.prepare("insert into tournament_summary (buy_in, prizepool, place, created_on) values($1,$2)")
-    insert(buy_in, prizepool, place, created_on)
+    insert = db.prepare("insert into tournament_summary (buy_in, prizepool, place, created_on) values($1,$2,$3,$4)")
+    insert(float(buy_in), float(prizepool), int(place), created_on)
--- parser.py
+++ parser.py
@@ -3,24 +3,46 @@ import datetime
 import fnmatch
 import postgresql
 import db_query
+import folder_path
 
 
 DB = postgresql.open(db_query.connection_string())
+FOLDER_PATH = folder_path.folder_path()
 
 
-file_name = "TS" + str(datetime.datetime.now().date().strftime("%Y%m%d")) + '*.txt'
-for file in os.listdir('.'):
-    if fnmatch.fnmatch(file, file_name):
-        file = open(file, mode='r', encoding='utf-8-sig')
-        lines = file.readlines()
-        file.close()
-        my_dict = {}
-        my_list = []
-        buy_in = lines[1][9:13]
-        prizepool = lines[3][19:23]
-        place = lines[-3][16:17]
-        date = lines[4][19:29]
-        format_str = '%Y/%m/%d'
-        date = datetime.datetime.strptime(date, format_str).date()
-        db_query.insert_data_into_db(buy_in, prizepool, place, date, DB)
+def convert_buy_in(buy_in):
+    if buy_in == '0.23':
+        buy_in = '0.25'
+    return buy_in
+
+
+def parser():
+    try:
+        file_name = "TS" + str(datetime.datetime.now().date().strftime("%Y%m%d")) + '*.txt'
+        for file in os.listdir(FOLDER_PATH):
+            if fnmatch.fnmatch(file, file_name):
+                file = open(os.path.join(FOLDER_PATH, file), mode='r', encoding='utf-8-sig')
+                lines = file.readlines()
+                file.close()
+                buy_in = convert_buy_in(lines[1][9:13])
+                prizepool = lines[3][19:23]
+                place = lines[-3][16:17]
+                date = lines[4][19:29]
+                format_str = '%Y/%m/%d'
+                date = datetime.datetime.strptime(date, format_str).date()
+                try:
+                    int(place)
+                except ValueError:
+                    continue
+                db_query.insert_data_into_db(buy_in, prizepool, place, date, DB)
+    except Exception as e:
+        print(e)
+
+
+
+parser()
+
+
+
+
 
